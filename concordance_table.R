083fafee97c8786b07153ddcc86e6b22c92d156c
roblanf <rob.lanfear@gmail.com>
2024-06-24 15:59:29 +1000

    larger pdf

--- concordance_table.R
+++ concordance_table.R
@@ -131,7 +131,7 @@ output <- create_heatmap(branch_id, concordance_vectors)
 
 # Save the plot to a PDF file
 pdf_filename <- paste0("concordance_table_", branch_id, ".pdf")
-ggsave(pdf_filename, plot = output$plot, width = 4, height = 2)
+ggsave(pdf_filename, plot = output$plot, width = 8, height = 4)
 
 # Save the long_data table to a CSV file
 csv_filename <- paste0("concordance_table_", branch_id, ".csv")
